efab93fd9bed56b5922477bb240081b37f7f434e
panytsch <romeo.crary@gmail.com>
2018-06-15 01:03:26 +0300

    I`ve not time to redirect but feth and POST done

--- src/Components/Big/FormAuth.js
+++ src/Components/Big/FormAuth.js
@@ -5,57 +5,77 @@ import axios from "axios";
 import Styles from "../../styles/FormStyles";
 import config from "../../Configs/mainConfig";
 
-const FormAuth = () => (
-  <Styles>
-    <h1>Welcome Back!</h1>
-    <Form
-      onSubmit={_values => {
-        console.log(_values);
-        axios
-          .get(`${config.host}users/authorize`, {
-            params: _values
-          })
-          .then(response => {
-            console.log(response);
-          });
-      }}
-      render={({ handleSubmit, form, submitting, pristine, values }) => (
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label>Login</label>
-            <Field
-              name="nickname"
-              component="input"
-              type="text"
-              placeholder="Nickname"
-            />
-          </div>
-          <div>
-            <label>Password</label>
-            <Field
-              name="password"
-              component="input"
-              type="password"
-              placeholder="Password"
-            />
-          </div>
-          <div className="buttons">
-            <button type="submit" disabled={submitting || pristine}>
-              Submit
-            </button>
-            <button
-              type="button"
-              onClick={form.reset}
-              disabled={submitting || pristine}
-            >
-              Reset
-            </button>
-          </div>
-          {/* <pre>{JSON.stringify(values, 0, 2)}</pre> */}
-        </form>
-      )}
-    />
-  </Styles>
+const FormAuth = props => (
+	<Styles>
+		<h1>Welcome Back!</h1>
+		<Form
+			onSubmit={_values => {
+				console.log(_values);
+				if (props.auth) {
+					axios
+						.get(`${config.host}users/authorize`, {
+							params: _values
+						})
+						.then(response => {
+							console.log(response);
+						});
+				} else {
+					console.log("post");
+					axios
+						.post(`${config.host}users/registration`, _values)
+						.then(response => {
+							console.log(response);
+						});
+				}
+			}}
+			render={({ handleSubmit, form, submitting, pristine, values }) => (
+				<form onSubmit={handleSubmit}>
+					<div>
+						<label>Login</label>
+						<Field
+							name="nickname"
+							component="input"
+							type="text"
+							placeholder="Nickname"
+						/>
+					</div>
+					<div>
+						<label>Password</label>
+						<Field
+							name="password"
+							component="input"
+							type="password"
+							placeholder="Password"
+						/>
+					</div>
+					{!props.auth && (
+						<div>
+							<label>Email</label>
+							<Field
+								name="email"
+								component="input"
+								type="email"
+								placeholder="Email"
+							/>
+						</div>
+					)}
+					<div className="buttons">
+						<button type="submit" disabled={submitting || pristine}>
+							Submit
+						</button>
+						<button
+							type="button"
+							onClick={form.reset}
+							disabled={submitting || pristine}
+						>
+							Reset
+						</button>
+					</div>
+					{/* <pre>{JSON.stringify(values, 0, 2)}</pre> */}
+				</form>
+			)}
+		/>
+	</Styles>
 );
 
 export default FormAuth;
--- src/Pages/AuthPage.js
+++ src/Pages/AuthPage.js
@@ -5,28 +5,28 @@ import { withRouter } from "react-router-dom";
 import FormAuth from "../Components/Big/FormAuth";
 
 class AuthPage extends React.Component {
-  componentWillMount() {
-    if (this.props.data.user && this.props.data.user.token) {
-      this.props.history.push(`/dashboard/${this.props.data.user.nickname}`);
-    }
-  }
-  render() {
-    //It's REDIREEEEECT!!!
-    // this.props.history.push("/");
-    return (
-      <div>
-        <FormAuth />
-      </div>
-    );
-  }
+	componentWillMount() {
+		if (this.props.data.user && this.props.data.user.token) {
+			this.props.history.push(`/dashboard/${this.props.data.user.nickname}`);
+		}
+	}
+	render() {
+		//It's REDIREEEEECT!!!
+		// this.props.history.push("/");
+		return (
+			<div>
+				<FormAuth auth={true} />
+			</div>
+		);
+	}
 }
 
 const mapDispatchToProps = dispatch => ({});
 
 const mapStateToProps = state => ({
-  data: state.userData
+	data: state.userData
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(
-  withRouter(AuthPage)
+	withRouter(AuthPage)
 );
